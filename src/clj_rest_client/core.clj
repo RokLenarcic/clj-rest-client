97cd9899055e1b28b0c67ddf2f905cde0830b45d
Rok Lenarcic <rok.lenarcic@gmail.com>
2018-09-24 11:19:46 +0200

    Add doc string to defrest

--- src/clj_rest_client/core.clj
+++ src/clj_rest_client/core.clj
@@ -71,7 +71,20 @@
 
 (s/fdef defrest-map :args (s/cat :def ::spec/terms :opts ::spec/options))
 
-(defmacro defrest [definition & {:keys [] :as args}]
+(defmacro defrest
+  "Defines multiple functions based on definition map. For map's structure see docs.
+
+  Valid definition parameters any of the following: map literal, a symbol (naming a var that resolves to a map), a string URL.
+  URL string can use `classpath:` or `file:` protocol.
+
+  Definition can be followed by opts key-value arguments, all of them are optional.
+
+  `:param-transform` This option specifies function that is transformation: parameter (symbol) -> query parameter name (string). Default `identity`.
+  `:json-bodies` If true then body parameters are sent as to-JSON serialized form params, otherwise body params are simply added to request as `:body`. Default true.
+  `:json-responses` If true then all requests specify `{:as :json}` and all responses are expected to be json responses. Default true.
+  `:instrument` Every function defined by `defrest` has its own `fdef` args spec. If instrument option is true, then all generated functions are also instrumented. Default true.
+  "
+  [definition & {:keys [] :as args}]
   `(defrest-map
      ~(cond
        (symbol? definition) (var-get (resolve definition))
